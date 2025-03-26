509ef2da88c55439fac4901757ca32ca1a7ef0ef
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-03-26 13:20:35 +0100

    Initial Commit

---
+++ PeripherySampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PeripherySampleApp
+//
+//  Created by Javier Calatrava on 26/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeripherySampleApp/PeripherySampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeripherySampleAppApp.swift
+//  PeripherySampleApp
+//
+//  Created by Javier Calatrava on 26/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PeripherySampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
